393aded3386d238e201f6a84ae53fc7a554c60b7
Hansi <s>
2022-02-25 22:54:55 +0530

    TravelMate

--- app/src/main/java/com/example/travelmate/araliyaActivity.kt
+++ app/src/main/java/com/example/travelmate/araliyaActivity.kt
@@ -2,10 +2,29 @@ package com.example.travelmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class araliyaActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_araliya)
+
+        val dataPicker = findViewById<DatePicker>(R.id.datapicker)
+        val today = Calendar.getInstance()
+        dataPicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH))
+        {
+                view,year,month,day ->
+            val month = month+1
+            val msg  = "You selected :$day/$month/$year"
+            Toast.makeText(this@araliyaActivity,msg,Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
--- app/src/main/java/com/example/travelmate/hotelsActivity.kt
+++ app/src/main/java/com/example/travelmate/hotelsActivity.kt
@@ -7,34 +7,28 @@ import android.widget.Button
 import android.widget.ImageView
 
 class hotelsActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        val imageView10 : ImageView = findViewById(R.id.imageView10)
-
-        //val search = findViewById<Button>(R.id.search)
-        val place1 = findViewById<Button>(R.id.place1)
-        val hotel1 : Button = findViewById(R.id.hotel1)
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotels)
 
+        val imageView10 : ImageView = findViewById(R.id.imageView10)
+        val place1 = findViewById<Button>(R.id.place1)
+        val hotel1 = findViewById<Button>(R.id.hotel1)
 
-        imageView10.setOnClickListener()
-        {
+        imageView10.setOnClickListener(){
             val intent = Intent(this,araliyaActivity::class.java)
             startActivity(intent)
         }
-
-        place1.setOnClickListener()
-        {
+        place1.setOnClickListener(){
             val intent = Intent(this,galleryActivity::class.java)
             startActivity(intent)
         }
-        hotel1.setOnClickListener()
-        {
+        hotel1.setOnClickListener(){
             val intent = Intent(this,hotelsActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
